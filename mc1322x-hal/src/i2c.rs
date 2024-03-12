4704ad079d9aabfed634f375b35a34855efd1a4c
SimonIT <simonit.orig@gmail.com>
2024-03-12 19:38:35 +0100

    Don't enable and disable on every transaction

--- mc1322x-hal/src/i2c.rs
+++ mc1322x-hal/src/i2c.rs
@@ -1,7 +1,52 @@
 use embedded_hal::i2c::{self, SevenBitAddress, I2c, Operation};
-use mc1322x_sys::{i2c_disable, i2c_enable, i2c_receiveinit, i2c_transmitinit};
+use mc1322x_sys::{i2c_disable, i2c_enable, I2C_NON_BLOCKING, i2c_receiveinit, i2c_transferred, i2c_transmitinit};
 
-pub struct I2c0;
+pub struct I2c0 {
+    enabled: bool,
+}
+
+impl I2c0 {
+    pub fn enable(&mut self) {
+        unsafe {
+            i2c_enable()
+        }
+        self.enabled = true;
+    }
+
+    pub fn disable(&mut self) {
+        unsafe {
+            i2c_disable()
+        }
+        self.enabled = false;
+    }
+
+    pub fn transferred(&mut self) -> bool {
+        let transferred = unsafe {
+            i2c_transferred()
+        };
+        transferred == 1
+    }
+
+    pub fn transmit(&mut self, address: u8, data: &[u8]) {
+        unsafe {
+            i2c_transmitinit(address, data.len() as _, data.as_ptr() as _)
+        }
+    }
+
+    pub fn receive(&mut self, address: u8, data: &mut [u8]) {
+        unsafe {
+            i2c_receiveinit(address, data.len() as _, data.as_mut_ptr() as _)
+        };
+    }
+}
+
+impl Drop for I2c0 {
+    fn drop(&mut self) {
+        if self.enabled {
+            self.disable();
+        }
+    }
+}
 
 #[derive(Debug, Copy, Clone, Eq, PartialEq)]
 pub enum Error {}
@@ -18,28 +63,23 @@ impl i2c::ErrorType for I2c0 {
 
 impl I2c<SevenBitAddress> for I2c0 {
     fn transaction(&mut self, address: u8, operations: &mut [Operation<'_>]) -> Result<(), Self::Error> {
-        unsafe {
-            i2c_enable()
-        }
         operations.iter_mut().for_each(|op|
             {
                 match op {
                     Operation::Read(buf) => {
-                        unsafe {
-                            i2c_receiveinit(address, buf.len() as _, buf.as_mut_ptr() as _)
-                        };
+                        self.receive(address, buf);
                     }
                     Operation::Write(buf) => {
-                        unsafe {
-                            i2c_transmitinit(address, buf.len() as _, buf.as_ptr() as _)
-                        }
+                        self.transmit(address, buf);
+                    }
+                }
+                if I2C_NON_BLOCKING == 1 {
+                    while !self.transferred() {
+                        // wait
                     }
                 }
             }
         );
-        unsafe {
-            i2c_disable()
-        }
         Ok(())
     }
 }
